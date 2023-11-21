9afd8ea9ad92ec303ea5b9689a319fd14a94f54b
milohontanosas <146100885+milohontanosas@users.noreply.github.com>
2023-11-21 09:00:48 -0500

    Add files via upload

---
+++ instructions.js
@@ -0,0 +1,39 @@
+// Get the container for instructions
+const instructionsContainer = document.getElementById('instructions-container');
+
+// 1. Wake up at 6:50 am
+const wakeUpTime = '6:50 AM';
+instructionsContainer.innerHTML += `<p>1. Wake up at ${wakeUpTime}</p>`;
+
+// 2. Brush teeth
+instructionsContainer.innerHTML += '<p>2. Brush teeth</p>';
+
+// 3. Change clothes
+instructionsContainer.innerHTML += '<p>3. Change clothes</p>';
+
+// 4. Gather books and clothes
+instructionsContainer.innerHTML += '<p>4. Gather books and clothes</p>';
+
+// 5. Leave house by 6:30
+const leaveHouseTime = '6:30 AM';
+instructionsContainer.innerHTML += `<p>5. Leave house by ${leaveHouseTime}</p>`;
+
+// 6. Drop off little sister to bus stop
+instructionsContainer.innerHTML += '<p>6. Drop off little sister to bus stop</p>';
+
+// 7. Drive to the cemetery
+instructionsContainer.innerHTML += '<p>7. Drive to the cemetery</p>';
+
+// 8. Talk and pray
+instructionsContainer.innerHTML += '<p>8. Talk and pray</p>';
+
+// 9. Leave by 7:10
+const leaveCemeteryTime = '7:10 AM';
+instructionsContainer.innerHTML += `<p>9. Leave by ${leaveCemeteryTime}</p>`;
+
+// 10. Head to the train station
+instructionsContainer.innerHTML += '<p>10. Head to the train station</p>';
+
+// 11. Go to class by 9 am
+const classTime = '9:00 AM';
+instructionsContainer.innerHTML += `<p>11. Go to class by ${classTime}</p>`;
